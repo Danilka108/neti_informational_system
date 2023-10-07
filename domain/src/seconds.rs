242f3007f83cca2fa7f99af1735f6691d3273310
danilka108 <danilka108@yandex.ru>
2023-10-08 01:16:52 +0700

    update Seconds type

--- domain/src/seconds.rs
+++ domain/src/seconds.rs
@@ -5,12 +5,12 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Hash, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Seconds {
-    value: u64,
+    pub val: u64,
 }
 
 #[derive(Serialize, Deserialize, Debug, Hash, Copy, Clone, PartialEq, Eq)]
 pub struct SecondsFromUnixEpoch {
-    value: Seconds,
+    pub seconds: Seconds,
 }
 
 impl SecondsFromUnixEpoch {
@@ -18,7 +18,7 @@ impl SecondsFromUnixEpoch {
         let duration: Seconds = SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
             .context("failed to get duration since unix epoch")?
-            .checked_add(Duration::from_secs(*ttl.as_ref()))
+            .checked_add(Duration::from_secs(ttl.val))
             .context("failed to compute issuted at for jwt token")?
             .as_secs()
             .into();
@@ -27,42 +27,31 @@ impl SecondsFromUnixEpoch {
     }
 
     pub fn is_expired(&self) -> Result<bool, anyhow::Error> {
-        let now: Seconds = SystemTime::now()
+        let now = SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
             .context("failed to get duration since unix epoch")?
-            .as_secs()
-            .into();
+            .as_secs();
 
-        Ok(now > self.value)
+        Ok(now > self.seconds.val)
     }
 }
 
 impl From<Seconds> for SecondsFromUnixEpoch {
-    fn from(value: Seconds) -> Self {
-        Self { value }
+    fn from(seconds: Seconds) -> Self {
+        Self { seconds }
+    }
+}
+
+impl From<u64> for SecondsFromUnixEpoch {
+    fn from(val: u64) -> Self {
+        Self {
+            seconds: Seconds { val },
+        }
     }
 }
 
 impl From<u64> for Seconds {
-    fn from(value: u64) -> Self {
-        Self { value }
-    }
-}
-
-impl AsRef<u64> for Seconds {
-    fn as_ref(&self) -> &u64 {
-        &self.value
-    }
-}
-
-impl AsRef<Seconds> for SecondsFromUnixEpoch {
-    fn as_ref(&self) -> &Seconds {
-        &self.value
-    }
-}
-
-impl AsRef<u64> for SecondsFromUnixEpoch {
-    fn as_ref(&self) -> &u64 {
-        &self.value.as_ref()
+    fn from(val: u64) -> Self {
+        Self { val }
     }
 }
