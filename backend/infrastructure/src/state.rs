430a0cf9cabe7f759ad5e58e4817dea322a2adbe
danilka108 <danilka108@yandex.ru>
2023-10-08 01:15:14 +0700

    update state

--- backend/infrastructure/src/state.rs
+++ backend/infrastructure/src/state.rs
@@ -1,34 +1,40 @@
-use crate::pg::init_pg_conn_pool;
-use crate::{
-    env_config::EnvConfig,
-    redis::{init_redis_conn_pool, RedisConnectionManager},
-    security::auth::gen_jwt_keys,
-};
+use std::sync::Arc;
+
+use app::api::{PasswordEncoder, SessionRepository, TokenManager, UserRepository};
+use app::auth::AuthService;
+use app::dyn_dependency;
+use app::session::SessionService;
+use app::token::TokenService;
+use app::user::UserService;
+
+use crate::env_config::EnvConfig;
+use crate::pg::{init_pg_conn_pool, PgSessionRepository, PgTransaction, PgUserRepository};
+use crate::security::password_encoder::Pbkdf2PassEncoder;
+use crate::security::token_manager::TokenManagerImpl;
 
 pub struct AppState {
     env_config: EnvConfig,
     pg_pool: sqlx::Pool<sqlx::Postgres>,
-    redis_pool: mobc::Pool<RedisConnectionManager>,
-    jwt_keys: (jsonwebtoken::EncodingKey, jsonwebtoken::DecodingKey),
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum AppStateError {
+    #[error("failed to init postgres connection pool")]
+    InitPgConnPoolError(#[from] sqlx::Error),
 }
 
 impl AppState {
     pub async fn new(env_config: EnvConfig) -> Result<Self, AppStateError> {
-        let pg_pool = init_pg_conn_pool(&env_config)
-            .await
-            .map_err(AppStateError::InitPgConnPool)?;
+        let pg_pool = init_pg_conn_pool(&env_config).await?;
 
-        let redis_pool = init_redis_conn_pool(&env_config)
-            .await
-            .map_err(AppStateError::InitRedisConnPool)?;
-
-        let jwt_keys = gen_jwt_keys(&env_config);
+        // let token_manager = Arc::new();
+        //
+        // let user_repository = Arc::new(PgUserRepository);
+        // let session_repository = Arc::new();
 
         Ok(Self {
             env_config,
             pg_pool,
-            redis_pool,
-            jwt_keys,
         })
     }
 
@@ -36,47 +42,52 @@ impl AppState {
         &self.pg_pool
     }
 
-    pub fn redis_pool(&self) -> &mobc::Pool<RedisConnectionManager> {
-        &self.redis_pool
-    }
-
-    pub fn jwt_decoding_key(&self) -> &jsonwebtoken::DecodingKey {
-        &self.jwt_keys.1
-    }
-
-    pub fn jwt_encoding_key(&self) -> &jsonwebtoken::EncodingKey {
-        &self.jwt_keys.0
-    }
-
     pub fn env_config(&self) -> &EnvConfig {
         &self.env_config
     }
-}
 
-#[derive(Debug)]
-pub enum AppStateError {
-    InitPgConnPool(sqlx::Error),
-    InitRedisConnPool(redis::RedisError),
-}
-
-impl std::fmt::Display for AppStateError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::InitPgConnPool(err) => {
-                write!(f, "failed to init postgres connection pool, cause {err}")
-            }
-            Self::InitRedisConnPool(err) => {
-                write!(f, "failed to init redis connecion pool, cause {err}")
-            }
-        }
+    pub fn password_encoder(&self) -> dyn_dependency!(PasswordEncoder) {
+        Box::new(Pbkdf2PassEncoder::new(self.env_config()))
     }
-}
 
-impl std::error::Error for AppStateError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            Self::InitPgConnPool(err) => Some(err),
-            Self::InitRedisConnPool(err) => Some(err),
-        }
+    pub fn token_manager(&self) -> dyn_dependency!(TokenManager) {
+        Box::new(TokenManagerImpl::new(self.env_config()))
+    }
+
+    pub fn session_repository(
+        &self,
+    ) -> dyn_dependency!(SessionRepository<Transaction = PgTransaction>) {
+        Box::new(PgSessionRepository)
+    }
+
+    pub fn user_repository(&self) -> dyn_dependency!(UserRepository<Transaction = PgTransaction>) {
+        Box::new(PgUserRepository)
+    }
+
+    pub fn token_service(&self) -> TokenService {
+        TokenService::new(
+            self.env_config.jwt_token_ttl_in_seconds.into(),
+            self.token_manager(),
+        )
+    }
+
+    pub fn session_service(&self) -> SessionService<PgTransaction> {
+        SessionService::new(
+            self.env_config.sessions_max_number_per_user,
+            self.env_config.session_ttl_in_seconds.into(),
+            self.session_repository(),
+        )
+    }
+
+    pub fn user_service(&self) -> UserService<PgTransaction> {
+        UserService::new(self.user_repository(), self.password_encoder())
+    }
+
+    pub fn auth_service(&self) -> AuthService<PgTransaction> {
+        AuthService::new(
+            self.session_service(),
+            self.user_service(),
+            self.token_service(),
+        )
     }
 }
